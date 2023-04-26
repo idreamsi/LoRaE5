32b6451f0e9d1376f542a60d4d864cdd534fa517
Infinite Dreams <8011806+idreamsi@users.noreply.github.com>
2023-04-26 11:16:32 +0330

    Update p2p_tx.ino

--- example/p2p_tx.ino
+++ example/p2p_tx.ino
@@ -1,9 +1,13 @@
 #include <LoRaE5.h>
 unsigned char buffer[128] = {0xef, 0xff, 0x55, 3, 4, 5, 6, 7, 8, 9,};
 
+//#define GROVE_TX 21
+//#define GROVE_RX 20
+
 void setup(void)
 {
     SerialUSB.begin(115200);
+    //lora.init(GROVE_RX, GROVE_TX);
     lora.init();
     lora.initP2PMode(866, SF12, BW125, 12, 15, 14);
 }
